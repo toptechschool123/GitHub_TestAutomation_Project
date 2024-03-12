e90a57c5173a11a553754e732e4bed45f515804e
Sayed <sadatsayedibrahim@gmail.com>
2024-03-11 18:38:31 -0700

    new

---
+++ src/main/java/GitHub_TestAutomation_Project/GitHub_TestAutomation_Project/App.java
@@ -0,0 +1,13 @@
+package GitHub_TestAutomation_Project.GitHub_TestAutomation_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
