d64579e3be51896643a44e3e9c28751b1d83b343
Sayed <toptechschool.sdet@gmail.com>
2024-03-16 14:15:38 -0700

    namge changed

--- src/test/java/selenium/Aziz_File.java
+++ src/test/java/selenium/Aziz_File.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 	
 	System.out.println("this is Aziz SDET 3");
 	
-	System.out.println("it is from sayed -sdet1 ");
+	System.out.println("it is from SAYED -sdet1 ");
 }
 
 }
