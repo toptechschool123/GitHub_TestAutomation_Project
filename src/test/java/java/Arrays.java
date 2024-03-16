f7dabc3b9b7cba14244daf9c225aec389d4e5394
Sayed <toptechschool.sdet@gmail.com>
2024-03-16 13:32:50 -0700

    added a class

---
+++ src/test/java/java/Arrays.java
@@ -0,0 +1,14 @@
+package java;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		System.out.println("HI IT IS TEST 1");
+		System.out.println("HI IT IS TEST 1");
+		
+		
+		System.out.println("HI IT IS TEST 1");
+		System.out.println("HI IT IS TEST 1");
+	}
+
+}
